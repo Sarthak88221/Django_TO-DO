048746ea2325d4134a7f77766e33076646f9100e
Sarthak Nirala <sarthaknirala2509@gmail.com>
2021-07-15 02:42:23 +0530

    performed CRUD with authentication and did mapping

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LogoutView
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete,CustomLoginView,RegisterPage
 
 urlpatterns = [
-    path('', views.taskList,name='tasks'),
+    path('login/',CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page = 'login'),name='logout'),
+    path('register/', RegisterPage.as_view(),name = 'register'),
+    
+    path('',TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='tasks'),
+    path('task-create/',TaskCreate.as_view(),name='tasks-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='tasks-update'),
+    path('task-delete/<int:pk>/',TaskDelete.as_view(),name='tasks-delete'),
 ]
